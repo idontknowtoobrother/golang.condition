e146445d720a7f6ef89a8c50b905b0fa19abf70a
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 19:09:45 +0700

    basic if else

--- index.go
+++ index.go
@@ -12,10 +12,21 @@ func main() {
 		result = "even number"
 	}
 
-	fmt.Printf("%d is %s", checkNumber, result)
+	fmt.Printf("\n%d is %s", checkNumber, result)
+
+	const a, b = -1, -15
+	fmt.Printf("\nmin of %d and %d is %d", a, b, findMin(a, b))
 
 }
 
 func isNumberIsOdd(number int) bool {
 	return number%2 == 1
+}
+
+func findMin(n1 int, n2 int) int {
+	if n1 < n2 {
+		return n1
+	}
+
+	return n2
 }
