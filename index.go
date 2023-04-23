bc7cfa4c84741bbf013853454c91e1519ecf61c9
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 19:14:58 +0700

    switch case

--- index.go
+++ index.go
@@ -17,6 +17,9 @@ func main() {
 	const a, b = -1, -15
 	fmt.Printf("\nmin of %d and %d is %d", a, b, findMin(a, b))
 
+	const getEnumCategory = "PROGRAMMING"
+	fmt.Printf("\nEnum of category %s is %d", getEnumCategory, getEnumOfCategory(getEnumCategory))
+
 }
 
 func isNumberIsOdd(number int) bool {
@@ -27,6 +30,20 @@ func findMin(n1 int, n2 int) int {
 	if n1 < n2 {
 		return n1
 	}
-
 	return n2
+}
+
+func getEnumOfCategory(category string) int {
+	switch category {
+	case "MATH":
+		return 1
+	case "SCIENCE":
+		return 2
+	case "LANGUAGE":
+		return 3
+	case "PROGRAMMING":
+		return 4
+	default:
+		return 0
+	}
 }
