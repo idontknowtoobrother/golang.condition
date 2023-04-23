70feba7586f0a5073b94a0556520fcfd172cdaf3
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-04-23 19:07:22 +0700

    init statment condition

--- index.go
+++ index.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	var result string
+	var checkNumber int = 10
+	if isOdd := isNumberIsOdd(checkNumber); isOdd {
+		result = "odd number"
+	} else {
+		result = "even number"
+	}
+
+	fmt.Printf("%d is %s", checkNumber, result)
+
+}
+
+func isNumberIsOdd(number int) bool {
+	return number%2 == 1
 }
